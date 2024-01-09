e086300e18f0d0bf3463e20354a11cb5fe384174
SadGhostYT <idanr2015@gmail.com>
2024-01-09 10:46:00 +0200

    fix: rolled back the lost files

--- app/src/main/java/dev/dirtyconcept/dishcover/screens/RegisterScreen.java
+++ app/src/main/java/dev/dirtyconcept/dishcover/screens/RegisterScreen.java
@@ -10,47 +10,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.Objects;
 
 import dev.dirtyconcept.dishcover.R;
 import dev.dirtyconcept.dishcover.utils.AnimationCommons;
 import dev.dirtyconcept.dishcover.utils.ValidationUtils;
 
-public class LoginScreen extends AppCompatActivity {
+public class RegisterScreen extends AppCompatActivity {
 
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
-        EditText emailField = AnimationCommons.setupField(this, R.id.login_email, R.string.email);
-        EditText passwordField = AnimationCommons.setupField(this, R.id.login_password, R.string.password);
+        EditText usernameField = AnimationCommons.setupField(this, R.id.register_username, R.string.username);
+        EditText emailField = AnimationCommons.setupField(this, R.id.register_email, R.string.email);
+        EditText passwordField = AnimationCommons.setupField(this, R.id.register_password, R.string.password);
+        EditText repasswordField = AnimationCommons.setupField(this, R.id.register_repassword, R.string.reenter_password);
 
-        Button loginButton = findViewById(R.id.login);
-        Button redirectRegisterButton = findViewById(R.id.redirect_register);
+        Button registerButton = findViewById(R.id.register);
+        Button redirectLoginButton = findViewById(R.id.redirect_login);
         TextView textViewErrorMessage = findViewById(R.id.error_message);
 
-        loginButton.setOnClickListener(v -> {
+        registerButton.setOnClickListener(v -> {
             textViewErrorMessage.setText("");
 
+            String username = usernameField.getText().toString();
             String email = emailField.getText().toString();
             String password = passwordField.getText().toString();
 
-            if (ValidationUtils.isEmpty(email, password)) {
+            if (ValidationUtils.isEmpty(username, email, password)) {
                 textViewErrorMessage.setText(R.string.empty_fields);
                 return;
             }
 
-            if (!isValidFormat(email, password)) {
+            if (!isValidFormat(username, email, password)) {
                 textViewErrorMessage.setText(R.string.wrong_format);
                 return;
             }
 
-            loginButton.setEnabled(false);
+            if (!Objects.equals(passwordField.getText().toString(), repasswordField.getText().toString())) {
+                textViewErrorMessage.setText(R.string.register_user_conflict);
+                return;
+            }
 
-            mAuth.signInWithEmailAndPassword(email, password)
+            registerButton.setEnabled(false);
+
+            mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, task -> {
                         if (task.isSuccessful() && task.getResult().getUser() != null) {
                             Intent main = new Intent(this, MainScreen.class);
@@ -59,32 +69,32 @@ public class LoginScreen extends AppCompatActivity {
                         } else {
                             handleLoginFailure(task.getException(), textViewErrorMessage);
                         }
-                        loginButton.setEnabled(true);
+                        registerButton.setEnabled(true);
                     });
         });
 
-        redirectRegisterButton.setOnClickListener(v -> {
+        redirectLoginButton.setOnClickListener(v -> {
             Intent register = new Intent(this, RegisterScreen.class);
             startActivity(register);
             finish();
         });
     }
 
-    private boolean isValidFormat(String email, String password) {
-        return ValidationUtils.isValidEmail(email) && ValidationUtils.isValidPassword(password);
+    private boolean isValidFormat(String username, String email, String password) {
+        return ValidationUtils.isValidUsername(username) &&
+                ValidationUtils.isValidEmail(email) &&
+                ValidationUtils.isValidPassword(password);
     }
 
     private void handleLoginFailure(Exception exception, TextView errorMessageTextView) {
         if (exception == null) {
             errorMessageTextView.setText(R.string.login_failed);
-        } else if (exception instanceof FirebaseAuthInvalidUserException) {
-            if (((FirebaseAuthInvalidUserException) exception).getErrorCode().equals("ERROR_USER_DISABLED")) {
-                errorMessageTextView.setText(R.string.login_account_disabled);
-            } else {
-                errorMessageTextView.setText(R.string.login_failed);
-            }
+        } else if (exception instanceof FirebaseAuthUserCollisionException) {
+            errorMessageTextView.setText(R.string.register_user_conflict);
         } else if (exception instanceof FirebaseTooManyRequestsException) {
-            errorMessageTextView.setText(getString(R.string.too_many_attempts, getString(R.string.login)));
+            errorMessageTextView.setText(getString(R.string.too_many_attempts, getString(R.string.register)));
+        } else {
+            errorMessageTextView.setText(R.string.register_failed);
         }
     }
 }
